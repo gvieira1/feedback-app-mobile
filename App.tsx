dac601f12f1932fbaec6f5454573f73484c05b88
Miranda <feaaar777@gmail.com>
2025-06-19 21:24:26 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RegisterScreen from './src/views/RegisterScreen';
+import LoginScreen from './src/views/LoginScreen';
 
 export default function App() {
-  return <RegisterScreen />;
+  return <LoginScreen />;
 }
