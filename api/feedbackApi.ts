9226c2b1313b8eadd7f02b45a330f75a133b0b52
Miranda <feaaar777@gmail.com>
2025-06-20 20:52:23 -0300

    Delete api directory

--- api/feedbackApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const feedbackApi = axios.create({
-  baseURL: 'http://192.168.0.122:8080/api', // seu IP + porta + prefixo da API
-  timeout: 5000,
-});
-
-export default feedbackApi;
