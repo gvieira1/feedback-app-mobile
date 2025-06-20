445e6ea55aaf1ba2bbf05523e6e175809d3dc7b0
Miranda <feaaar777@gmail.com>
2025-06-20 17:20:00 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,5 +3,5 @@ import RegisterScreen from './src/views/RegisterScreen';
 import LoginScreen from './src/views/LoginScreen';
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
